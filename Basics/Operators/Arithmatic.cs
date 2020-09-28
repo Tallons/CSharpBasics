6d10b188c2eb5498863025f6815f067ce9859fd6
Tallons <Tallons27@yahoo.com>
2020-09-27 23:58:54 -0600

    update

--- Basics/Operators/Arithmatic.cs
+++ Basics/Operators/Arithmatic.cs
@@ -14,6 +14,12 @@ namespace Basics
             Console.WriteLine("Division: " + (num1 / num2));
             Console.WriteLine("Modulus: " + (num1 % num2));
             Console.WriteLine(num1 + " Squared: " + Math.Pow(num1, 2));
+            Console.WriteLine(num1 + " Square Root: " + Math.Sqrt(num1));
+            Console.WriteLine(50.5 + " Round: " + Math.Round(50.5, 0, MidpointRounding.AwayFromZero));
+            Console.WriteLine(50.5 + " Round up: " + Math.Ceiling(50.5));
+            Console.WriteLine(50.5 + " Round down: " + Math.Floor(50.5));
+            Console.WriteLine(" Minimum: " + Math.Min(num1, num2));
+            Console.WriteLine(" Maximum: " + Math.Max(num1, num2));
             Console.ResetColor();
 
         }
