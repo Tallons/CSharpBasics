9ba1902eb13fd4f7f32f40f2c1af934126404f76
Tallons <Tallons27@yahoo.com>
2020-09-29 23:56:33 -0600

    update

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿using Basics;
+using System;
 
 namespace Basics {
     class Program {
         public static void main() {
             string name = "Brandon";
             int age = 25;
-            Console.WriteLine("{0} is {1}", name, age);
+            Console.WriteLine($"{name} is {age}");
+            //Console.WriteLine("{0} is {1}", name, age);
 
             Arithmatic.arithmatic(15,12);
-             
+            Conditional.conditional(15, "developer");
+            Strings.strings();
         }
     }
 }
