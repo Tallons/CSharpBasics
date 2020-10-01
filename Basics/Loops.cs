e0aa8b2c52936eff3ed24731cfb19e5e74363cd8
Tallons <Tallons27@yahoo.com>
2020-09-30 19:53:52 -0600

    loops update

---
+++ Basics/Loops.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basics {
+    class Loops {
+        public static void loops(string[] names) {
+            int NUM = 5;
+            BasicForLoop(NUM);
+            ForEachLoop(names);
+            WhileLoop(names);
+            DoWhileLoop(NUM);
+
+
+        }
+        static void BasicForLoop(int num) {
+            for (int i = 0; i < num; i++) {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void ForEachLoop(string[] names) {
+            foreach (string name in names) {
+                Console.WriteLine(name);
+            }
+        }
+
+        static void WhileLoop(string[] names) {
+            int i = 0;
+            while (i < names.Length) {
+                Console.WriteLine($"{i}: {names[i]}");
+                i++;
+            }
+            
+        }
+        static void DoWhileLoop(int num) {
+            int i = 0;
+            do {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < num);
+        }
+    }
+}
