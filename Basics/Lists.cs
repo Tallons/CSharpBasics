8a109bca945abc10386b7ec7e15be320209d5c84
Noah Orr <Tallons27@yahoo.com>
2020-11-09 00:21:52 -0700

    Lists

---
+++ Basics/Lists.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBasics.Basics
+{
+	class Lists
+	{
+		public void main()
+		{
+			List<double> grades = new List<double>() {92.5, 97.6, 82.7};
+			grades.Add(85.0);
+			double firstItem = grades[0];
+
+			var result = 0.0;
+			foreach (var number in grades)
+			{
+				result += number;
+			}
+			Console.WriteLine($"Average Grade: {(result / grades.Count):N3}");
+
+		}
+		
+
+	}
+}
